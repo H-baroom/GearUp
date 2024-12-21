a058ad28d9aafa05a78af88f2f9f0059844fd565
Hashim Baroom <hashim.m.baroom@gmail.com>
2024-12-21 12:51:10 +0300

    Update RentingRequestDTO_In.java

--- capston3/InDTO/RentingRequestDTO_In.java
+++ capston3/InDTO/RentingRequestDTO_In.java
@@ -1,6 +1,8 @@
 package com.example.capston3.InDTO;
 
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -10,15 +12,10 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class RentingRequestDTO_In {
 
-
-    private Integer renting_id;
-
-    private Integer user_id;
-
-   // @FutureOrPresent(message = "Start date must be in the present or future")
+   @FutureOrPresent(message = "Start date must be in the present or future")
     private LocalDate startDate;
 
-  //  @Future(message = "End date must be in the future")
+   @Future(message = "End date must be in the future")
     private LocalDate endDate;
 
     private Integer motorcycleId ;
